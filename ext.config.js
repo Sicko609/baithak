66f5cd6fe40d5897cbf41b23f535f6c719121d2d
Sicko <tauhidnoman1@gmail.com>
2026-08-10 16:31:48 +0600

    Add files via upload

---
+++ ext.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore eslint errors during builds
+    ignoreDuringBuilds: true,
+  }
+};
+
+module.exports = nextConfig;
